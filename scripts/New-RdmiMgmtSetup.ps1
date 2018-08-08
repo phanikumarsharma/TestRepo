599342721f6a4bb8e9e4d12cf33a2a708590c229
phanikumarsharma <ramaduguphanikumar@gmail.com>
2018-08-08 06:49:10 -0700

    files commit

--- scripts/New-RdmiMgmtSetup.ps1
+++ scripts/New-RdmiMgmtSetup.ps1
@@ -1,15 +1,11 @@
 ﻿<#
-
 .Synposys
 Deploy a new RDmi Management Console in Azure
-
 .Description
 This script is used to provision a new RDmi Management Web Portal in Azure. It creates
 two App services- Api and App. At End of this script, it will generate public URL of Web Portal.
-
 .Permission
 Administrator
-
 #>
 
 
@@ -82,8 +78,6 @@ Param(
     [Parameter(Mandatory = $False)]
     [ValidateNotNullOrEmpty()]
     [string]$ApiAppExtractionPath = ".\msft-rdmi-saas-api\msft-rdmi-saas-api.zip"
-
-
 )
 
 function Disable-ieESC {
@@ -114,7 +108,7 @@ try
         Write-Output "AzureRM module Not Available. Installing AzureRM Module"
         Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
         Install-Module Azure -Force
-        Install-Module AzureRm -Force 
+        Install-Module AzureRm -Force
         Write-Output "Installed AzureRM Module successfully"
     } 
     else
@@ -126,9 +120,7 @@ try
 
     Write-Output "Importing AzureRm Module.."
     Import-Module AzureRm -ErrorAction SilentlyContinue -Force
-    Import-Module AzureRM.profile
-    Import-Module AzureRM.resources
-    Import-Module AzureRM.Compute
+
     #Login to AzureRM Account
 
     Write-Output "Login Into Azure RM.."
@@ -156,7 +148,7 @@ try
         Write-Output "Resource group with name $ResourceGroupName has been created"
     }
     elseif($ResourceGroup)
-    {
+        {
             try
             {
                 ##################################### APPSERVICE PLAN #####################################
@@ -278,8 +270,9 @@ try
                                     "ResourceUrl" = "$ResourceURL";
                                     "RedirectURI" = "https://"+"$WebUrl"+"/";
                                     }
-                $Redirecturl1="https://"+"$WebUrl"+"/"
+                $Redirecturl1="http://"+"$WebUrl"+"/"
                 $Redirecturl2="https://login.microsoftonline.com/common/oauth2/logout?post_logout_redirect_uri="
+                $ApplicationID="42266145-f820-43e4-b951-2b3a68530f71"
                 $ADapplication=Get-AzureRmADApplication -ApplicationId $ApplicationID
                 $add=$ADapplication.ReplyUrls.Add($Redirecturl1)
                 $add=$ADapplication.ReplyUrls.Add("$Redirecturl2"+"$Redirecturl1")
@@ -320,7 +313,7 @@ try
                 #Change the Url in the main.bundle.js file with the with ApiURL
 
                 Write-Output "Updating the Url in main.bundle.js file with Api-app Url"
-                (Get-Content $MainbundlePath).replace( "[api_url]", "https://"+$ApiUrl) | Set-Content $MainbundlePath
+                (Get-Content $MainbundlePath).replace( "[api_url]", "http://"+$ApiUrl) | Set-Content $MainbundlePath
 
                 #Get publishing profile from web app
                 
@@ -371,12 +364,11 @@ try
                 Write-Output $_.Exception.Message
             }
 
-            Write-Output "Api URL : https://$ApiUrl"
-            Write-Output "Web URL : https://$WebUrl"
+            Write-Output "Api URL : http://$ApiUrl"
+            Write-Output "Web URL : http://$WebUrl"
        }
     }
-
-    New-PSDrive -Name RemoveRG -PSProvider FileSystem -Root "C:\msft-rdmi-saas-offering\msft-rdmi-saas-offering" | Out-Null
+New-PSDrive -Name RemoveRG -PSProvider FileSystem -Root "C:\msft-rdmi-saas-offering\msft-rdmi-saas-offering" | Out-Null
 @"
 <RemoveRG>
 <SubscriptionId>$SubscriptionId</SubscriptionId>
@@ -394,8 +386,7 @@ try
      $trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).Date -RepetitionInterval $repeat -RepetitionDuration $duration
      $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd
      Register-ScheduledTask -TaskName $jobname -Action $action -Trigger $trigger -RunLevel Highest -User "system" -Settings $settings
-
-   }
+}
 catch [Exception]
 {
     Write-Output $_.Exception.Message
